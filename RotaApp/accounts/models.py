f606c22d9b6a11e6866d151057197e566bedbc34
Arnab Chakraborty <arnab033chakraborty@gmail.com>
2020-01-02 15:08:32 +0530

    Added member model

--- RotaApp/accounts/models.py
+++ RotaApp/accounts/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+ROLE_CHOICES = (
+		('ADMIN', 'Admin'),
+		('MEMBER', 'Member'),
+	)
+
+class Member(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+	phone = models.CharField(max_length=15)
+	address = models.TextField()
+	date_of_birth = models.DateField()
+	date_of_joining = models.DateField()
+	blood_group = models.CharField(max_length=5)
+	designation = models.CharField(max_length=100)
