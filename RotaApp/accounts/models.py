88d6ba37dbda2445612486a928a13f8bd4643be6
Arnab Chakraborty <arnab033chakraborty@gmail.com>
2020-01-02 15:18:43 +0530

    Made phone number a unique field

--- RotaApp/accounts/models.py
+++ RotaApp/accounts/models.py
@@ -9,7 +9,7 @@ ROLE_CHOICES = (
 class Member(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
-	phone = models.CharField(max_length=15)
+	phone = models.CharField(max_length=15, unique=True)
 	address = models.TextField()
 	date_of_birth = models.DateField()
 	date_of_joining = models.DateField()
